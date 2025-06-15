493df22a68874e7a3eb4c0571bdddd27fec13d42
yasin <swsbbnml@gmail.com>
2025-06-15 20:24:14 +0300

    v-13

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,11 @@
-"use client";
-
-// import type { Metadata } from "next";
+import type { Metadata } from "next";
 import "./globals.scss";
 import "../../public/styles/utils/styles.scss";
-// import { Metadata } from "next";
-/*
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});*/
-
-// export const metadata: Metadata = {
-//   title: "codereview",
-//   description: "какое то описание...",
-// };
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <html lang="en" data-lt-installed="true" suppressHydrationWarning={true}>
-//       <body /*className={`${geistSans.variable} ${geistMono.variable}`}*/>{children}</body>
-//     </html>
-//   );
-// }
+export const metadata: Metadata = {
+  title: "codereview",
+  description: "какое то описание...",
+};
 
 export default function RootLayout({
   children,
